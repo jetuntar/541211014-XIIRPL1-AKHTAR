ddfc07dc3e94fd8c67ec228dbb70568454bc1f3a
Tartar250 <110464136+jetuntar@users.noreply.github.com>
2023-10-24 17:33:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const userrouter = require('./router/users')
+
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(userrouter)
+
+app.listen(port, () => {
+  console.log(`This server is running in port ${port}`)
+})
