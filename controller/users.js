2503e3105ae4fde556d474ee345c7204afbf1c41
jetuntar <akhtarziyada1@gmail.com>
2023-10-31 19:23:03 +0700

    express js #14 menambahkan data

--- controller/users.js
+++ controller/users.js
@@ -21,30 +21,20 @@ module.exports = {
       res.status(400).json({sucess: false})
     }
 
-    if(users.length > 0){
-      res.json({
-        status: true,
-        data: users,
-        method: req.method,
-        url: req.url
-      })
-    }else{
-      res.json({
-        status: false,
-        message: "Data masih kosong"
-      })
-    }
-    res.json(users)
   },
-  store: (req, res) => {
-    users.push(req.body)
-    res.json({
-      status: true,
-      data: users,
-      method: req.method,
-      url: req.url,
-      message: "Data berhasil ditambahkan"
-    })
+  store: async (req, res) => {
+    try {
+      const user = await User.create(req.body)
+      res.status(200).json({
+        status: true,
+        data: user,
+        method: req.method,
+        url: req.url,
+        message: "Data berhasil ditambahkan"
+      })
+    } catch (error) {
+      res.status(400).json({success: false})
+    }    
   },
   update: (req, res) => {
     const id = req.params.id
